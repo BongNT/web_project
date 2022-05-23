8d7208c520d039920e648b45e67f16651a56c1d2
Nguyễn Trung Hiếu <63243790+hieu404@users.noreply.github.com>
2022-05-23 23:34:37 +0700

    add editing and removing data

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Toolbar() {
+	const [selected, setSelected] = useState(false);
+	function createDelBtn() {
+		if (
+			document.querySelector(
+				"#content .MuiDataGrid-footerContainer > div:first-child"
+			).innerHTML != ""
+		) {
+			setSelected(true);
+		} else {
+			setSelected(false);
+		}
+	}
 	return (
-		<Stack
-			direction="row"
-			spacing={2}
-			className="justify-content-center border-bottom w-100 p-3"
-		>
+		<Stack direction="row" spacing={60} className="pt-3 pb-3">
+			<Button variant="outline" disabled={selected} className="" vis>
+				<DeleteIcon />
+			</Button>
 			<Button variant="contained">
 				<AddIcon /> Thêm mới
-			</Button>
-			<Button variant="contained">Chỉnh sửa</Button>
-			<Button variant="contained" href="/add">
-				Link
 			</Button>
 		</Stack>
 	);
